440a0a1ae7fad5ccdeec6b7beb96e9c48a2bfd93
Fabrice Dufils Siyapdje <dev.dafab@gmail.com>
2020-09-05 20:18:48 +0200

    styling

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const countDownDate = new Date("09.11.2020 8:30:00").getTime();
+function countdown() {
+    const countDownDate = new Date("09.11.2020 8:30:00").getTime();
 
-function navigate(path) {
-    window.location = `${window.location.href}#/${path}`;
+    const x = setInterval(function () {
+        const now = new Date().getTime();
+        // Find the distance between now and the count down date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        const countdown = document.querySelector("#countdown");
+
+        countdown.innerHTML = `${days} jours ${hours}h:${minutes}m:${seconds}s`;
+
+        if (distance < 0) {
+            clearInterval(x);
+            countdown.innerHTML = "EXPIRED";
+        }
+    }, 1000);
 }
-
-const x = setInterval(function () {
-    const now = new Date().getTime();
-    // Find the distance between now and the count down date
-    const distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    const countdown = document.querySelector("#countdown");
-
-    countdown.innerHTML = `${days} jours ${hours}h:${minutes}m:${seconds}s`;
-
-    if (distance < 0) {
-        clearInterval(x);
-        countdown.innerHTML = "EXPIRED";
-    }
-}, 1000);
 
 
 const HomeComponent = {
     render: () => {
         return `
-<div id="bg-landing-page" class="bg-landing-page content">
+<div id="bg-landing-page" class="font-bold bg-landing-page content">
 <div class="h-full flex flex-col w-full">
         <div class="-px-4 -py-4 flex flex-col ml-auto mr-auto md:flex-row">
             <div class="jeanne-eric-title flex ml-auto mr-auto font-bold">Jeanne</div>
@@ -43,7 +41,7 @@ const HomeComponent = {
         onclick="navigate('programme')"  type="button">
           Programme
         </button>
-        <div id="countdown" class="color-mintcream font-bold ml-auto mr-auto mt-auto">FFFFF</div>
+        <div id="countdown" class="color-mintcream font-bold ml-auto mr-auto mt-auto"></div>
         </div>
  </div>
 </div>
@@ -53,8 +51,9 @@ const HomeComponent = {
 const ProgrammComponent = {
     render: () => {
         return `
-<div class="flex text-red-500 bg-programme-page flex-col">
-<h3 class="entry-font-family text-2xl mt-16 text-center">
+<div class="bg-programme-page font-bold content">
+<div class="flex text-red-500  flex-col h-full">
+<h3 class="entry-font-family text-2xl mt-4 text-center">
     Nous avons le plaisir de vous convier à notre mariage
     qui aura lieu le vendredi 11 septembre 2020.
 </h3>
@@ -73,9 +72,10 @@ const ProgrammComponent = {
 <ul>
 </div>
 
-<div class="absolute bottom-0 entry-font-family text-2xl programm-footer text-center">
+<div class="entry-font-family text-2xl text-center ml-auto mr-auto mt-auto">
 Merci de partarger avec nous ce joyeux événement sur un
 thème chic et tendance.
+</div>
 </div>
 </div>
         `;
@@ -89,12 +89,18 @@ const routes = [
 
 const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
 const findComponentByPath = (path, routes) => routes.find(r => r.path.match(new RegExp(`^\\${path}$`, 'gm'))) || undefined;
+const navigate = (path) => window.location = `${window.location.href}#/${path}`;
 
 const router = () => {
     // Find the component based on the current path
     const path = parseLocation();
     // If there's no matching route, get the "Error" component
     const {component = HomeComponent} = findComponentByPath(path, routes) || {};
+    if (component === HomeComponent) {
+        debugger;
+        countdown();
+    }
+
     document.getElementById('app').innerHTML = component.render();
 };
 
